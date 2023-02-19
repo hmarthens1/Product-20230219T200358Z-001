6fd7713adaa81dac8a5cc745c11c94b2c566a9cf
hmarthens1 <55443436+hmarthens1@users.noreply.github.com>
2023-02-19 15:04:42 -0800

    Update connection.php

--- Product/Log in/public/connection.php
+++ Product/Log in/public/connection.php
@@ -4,10 +4,8 @@ $dbuser = "b1ebb89a30fd7f";
 $dbpass = "937540e9";
 $dbname = "heroku_0184b4aca408eb5";
 
-// if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
-//     die("failed to connect!");
-// }
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
+    die("failed to connect!");
+}
 
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-?>
