26e6950f36cde5755774a182e8b3753885fd8fb4
hmarthens1 <55443436+hmarthens1@users.noreply.github.com>
2023-02-19 17:18:19 -0800

    Update index.php

--- Product/index.php
+++ Product/index.php
@@ -1,8 +1,8 @@
 <?php 
 session_start();
 
-include(__DIR__ . "/Log in/public/connection.php");
-include(__DIR__ . "/Log in/public/functions.php");
+include("/Log in/public/connection.php");
+include("/Log in/public/functions.php");
 
 $user_data = check_login($con);
 
