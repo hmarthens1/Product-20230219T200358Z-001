d1ec81b224ffe526b9afa09aeddfa2abe122f416
hmarthens1 <55443436+hmarthens1@users.noreply.github.com>
2023-02-19 17:25:41 -0800

    Add files via upload

---
+++ Product/Log in/public/connection.php
@@ -0,0 +1,11 @@
+<?php
+$dbhost = "us-cdbr-east-06.cleardb.net";
+$dbuser = "b1ebb89a30fd7f";
+$dbpass = "937540e9";
+$dbname = "heroku_0184b4aca408eb5";
+
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
+    die("failed to connect!");
+}
+
+?>
